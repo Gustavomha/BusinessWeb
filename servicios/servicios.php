b83f97c24538c5aca779cb6db9b2f57d82d70bad
Gustavomha <gustavomha.1999@gmail.com>
2021-04-16 20:30:47 -0400

    16-04-2021

--- servicios/servicios.php
+++ servicios/servicios.php
@@ -1,127 +1,132 @@
 <!DOCTYPE HTML>
 <html lang="es">
-    <head>
-        <meta charset="utf-8" />
-        <meta name="viewport" content="width=device-width, user-scalable=no" />
-        <title>Servicios Fabrica</title>
-        <link rel="stylesheet" href="../bootstrap/bootstrap/css/bootstrap.min.css">
-        <link rel="stylesheet" type="text/css" href="../assets/estilos_servicios.css" />
-        <link rel="icon" type="favicon/x-icon" href="../assets/img/logo_fabrica.svg" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link href="https://fonts.googleapis.com/css2?family=Lora:ital@1&family=Merriweather:ital,wght@1,300&family=Roboto:wght@300&display=swap" rel="stylesheet">
-    </head>
 
-    <body>
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, user-scalable=no" />
+    <title>Servicios Fabrica</title>
+    <link rel="stylesheet" href="../bootstrap/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="../assets/estilos_servicios.css" />
+    <link rel="icon" type="favicon/x-icon" href="../assets/img/logo_fabrica.svg" />
+    <link rel="stylesheet" href="../assets/responsives/responsive-services.css"/>
+    <link rel="preconnect" href="https://fonts.gstatic.com" />
+    <link href="https://fonts.googleapis.com/css2?family=Lora:ital@1&family=Merriweather:ital,wght@1,300&family=Roboto:wght@300&display=swap" rel="stylesheet">
+</head>
 
-        <!--INICIO DEL HEADER -->
-        <header class="header_services">
+<body>
 
-            <div class="logo_description">
-                <h3 a href="http://localhost/masterphp/Fabrica.cl/">FABRICA TECHNOLOGY</h3>
-            </div>
+    <!--INICIO DEL HEADER -->
+    <header class="header_services">
 
-            <nav id="nav_services">
+        <div class="logo_description">
+            <h3 a href="http://localhost/masterphp/Fabrica.cl/">FABRICA TECHNOLOGY</h3>
+        </div>
+
+        <nav id="nav_services">
+            <ul>
+                <li>
+                    <a href="http://localhost/masterphp/Fabrica.cl">Inicio</a>
+                </li>
+                <li>
+                    <a href="http://localhost/masterphp/Fabrica.cl/servicios/servicios.php">Servicios</a>
+                </li>
+                <li>
+                    <a href="http://localhost/masterphp/Fabrica.cl/#Tiempos">Tiempos</a>
+                </li>
+                <li>
+                    <a href="http://localhost/masterphp/Fabrica.cl/#contacto">Contacto</a>
+                </li>
+                <li>
+                    <a href="http://localhost/masterphp/Fabrica.cl/#proyectos">proyectos</a>
+                </li>
+
+            </ul>
+        </nav>
+    </header>
+
+
+
+    <div class="presentation">
+        <a href="http://localhost/masterphp/Fabrica.cl/servicios/servicios.php">
+            <h1>SERVICIOS CORPORATIVOS</h1>
+        </a>
+        <h4>SOLUCIONES DE ENTORNO TI</h4>
+    </div>
+
+    <!--CONTAINER SERVICES-->
+
+    <div class="services">
+
+        <h2>SERVICIOS TECNOLOGICOS</h2>
+        <P class="description-services">
+            Ofrecemos una cadena tecnica continua desde la creacion de imagenes coorporativas pasando por procesos
+            de replicacion en equipos e instalacion en instacias de nuestros clientes, gestion logistica e implementacion
+            de sistemas intelingentes para el seguimiento del proceso.
+        </P>
+
+        <br><br>
+        <P class="description-services">
+            Fabrica Inc se caracteriza por ser una empresa que brinda atencion especializadas hacia nuestros clientes
+            basandonos en los mas altos entandares por mas de 10 años en cientos de proyectos tecnologicos.
+        </P>
+        <img src="../assets/img/servicios2.png" alt="imagen/services">
+
+    </div>
+    <!--TIPS SERVICES-->
+    <div class="tips_services">
+        <div class="tips">
+            <h5>HARDWARE</h5>
+            <nav>
                 <ul>
                     <li>
-                        <a href="http://localhost/masterphp/Fabrica.cl">Inicio</a>
+                        Manejo Especializado
                     </li>
                     <li>
-                        <a href="http://localhost/masterphp/Fabrica.cl/servicios/servicios.php">Servicios</a>
+                        Instalaciones y actualizaciones
                     </li>
                     <li>
-                        <a href="http://localhost/masterphp/Fabrica.cl/#Tiempos">Tiempos</a>
+                        servicio confiable
                     </li>
-                    <li>
-                        <a href="http://localhost/masterphp/Fabrica.cl/#contacto">Contacto</a>
-                    </li>
-                    <li>
-                        <a href="http://localhost/masterphp/Fabrica.cl/#proyectos">proyectos</a>
-                    </li>
-
                 </ul>
             </nav>
-        </header>
-
-
-
-        <div class="presentation">
-            <a href="http://localhost/masterphp/Fabrica.cl/servicios/servicios.php"><h1>SERVICIOS CORPORATIVOS</h1></a>
-            <h4>SOLUCIONES DE ENTORNO TI</h4>
         </div>
 
-        <!--CONTAINER SERVICES-->
-
-        <div class="services">
-           
-            <h2>SERVICIOS TECNOLOGICOS</h2>
-            <P class="description-services">
-                Ofrecemos una cadena tecnica continua desde la creacion de imagenes coorporativas pasando por procesos
-                de replicacion en equipos e instalacion en instacias de nuestros clientes, gestion logistica e implementacion
-                de sistemas intelingentes para el seguimiento del proceso.
-            </P>
-           
-            <br><br>
-            <P class="description-services">
-                Fabrica Inc se caracteriza por ser una empresa que brinda atencion especializadas hacia nuestros clientes
-                basandonos en los mas altos entandares por mas de 10 años en cientos de proyectos tecnologicos.
-            </P>
-            <img src="../assets/img/servicios2.png" alt="imagen/services">
-
-        </div>
-        <!--TIPS SERVICES-->
-        <div class="tips_services">
-            <div class="tips">
-                <h5>HARDWARE</h5>
-                <nav>
-                    <ul>
-                        <li>
-                            Manejo Especializado
-                        </li>
-                        <li>
-                            Instalaciones y actualizaciones
-                        </li>
-                        <li>
-                            servicio confiable
-                        </li>
-                    </ul>
-                </nav>
-            </div>
-
-            <div class="tips">
-                <h5>SOFTWARE</h5>
-                <nav>
-                    <ul>
-                        <li>
-                            Soluciones en programas
-                        </li>
-                        <li>
-                            Creacion de imagenes
-                        </li>
-                    </ul>
-                </nav>
-            </div>
-
-            <div class="tips">
-                <h5>Instalacion</h5>
-                <nav>
-                    <ul>
-                        <li>
-                            Instalacion en terreno
-                        </li>
-                        <li>
-                            Actualizaciones al dia
-                        </li>
-                        <li>
-                            Personal calificado
-                        </li>
-                    </ul>
-                </nav>
-            </div>
+        <div class="tips">
+            <h5>SOFTWARE</h5>
+            <nav>
+                <ul>
+                    <li>
+                        Soluciones en programas
+                    </li>
+                    <li>
+                        Creacion de imagenes
+                    </li>
+                </ul>
+            </nav>
         </div>
 
+        <div class="tips">
+            <h5>Instalacion</h5>
+            <nav>
+                <ul>
+                    <li>
+                        Instalacion en terreno
+                    </li>
+                    <li>
+                        Actualizaciones al dia
+                    </li>
+                    <li>
+                        Personal calificado
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    </div>
 
 
-        <!--slider-->
+
+    <!--slider-->
+    <div id="slider-container">
         <div class="carousel_services">
             <h2>Servicio de instalacion</h2>
             <div id="slider" class="carousel slide mb-4" data-ride="carousel">
@@ -159,7 +164,7 @@
                         <img class="d-block img-fluid" src="../assets/img/slider4.png" />
                         <div class="carousel-caption">
                             <h3>Imagenes operativas empresariales</h3>
-                            <p>Diseño de imagenes basadas en los estandares solicitado por el cliente, para 
+                            <p>Diseño de imagenes basadas en los estandares solicitado por el cliente, para
                                 satisfaccion de sus necesidades laborales
                             </p>
                             <a href="#">ver mas</a>
@@ -174,141 +179,142 @@
 
             </div>
         </div>
+    </div>
 
-        <div class="networks">
-            <h1>SOLUCIONES DE REDES Y SERVIDORES</h1>
+    <div class="networks">
+        <h1>SOLUCIONES DE REDES Y SERVIDORES</h1>
 
-            <img src="../assets/img/migrate.png">
-            <div class="networks_installation">
-                <h2>Gestionamos y recomendamos proyectos para tu empresa, oficina y todo lugar</h2>
-                <p>Gestionamos y te asesoramos en instalaciones para tu empresa oficina o cualquier entidad necesaria
-                    Nos basamos en los mas altos estandares de instalaciones como certificaciones Cisco, y experiencia
-                    en el area<br><br>
-                    <a href="http://localhost/masterphp/Fabrica.cl/servicios/redes.php">Leer mas sobre el tema</a>
-                </p>
+        <img src="../assets/img/migrate.png">
+        <div class="networks_installation">
+            <h2>Gestionamos y recomendamos proyectos para tu empresa, oficina y todo lugar</h2>
+            <p>Gestionamos y te asesoramos en instalaciones para tu empresa oficina o cualquier entidad necesaria
+                Nos basamos en los mas altos estandares de instalaciones como certificaciones Cisco, y experiencia
+                en el area<br><br>
+                <a href="http://localhost/masterphp/Fabrica.cl/servicios/redes.php">Leer mas sobre el tema</a>
+            </p>
+        </div>
+
+        <div class="nav_networks">
+            <ul>
+                <li>
+                    Manejo Especializado
+                </li>
+                <li>
+                    Instalaciones y actualizaciones
+                </li>
+                <li>
+                    Basados en normas Osi
+                </li>
+                <li>
+                    Certificaciones cisco
+                </li>
+                <li>
+                    Confianza y calidad
+                </li>
+            </ul>
+        </div>
+    </div>
+
+    <div class="container">
+
+        <div class="row">
+            <div class="col-md-6">
+                <div class="card">
+                    <img src="../assets/img/slider2.png" class="img-responsive" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Servicios electricos e instalaciones</h5>
+                        <p class="card-text">capacidades técnicas para dar una pronta solución, orientando, evaluando y ejecutando los proyectos de instalación
+                            de redes y comunicaciones que necesites</p>
+                        <a href="http://localhost/masterphp/Fabrica.cl/servicios/redes.php">leer Descripcion</a>
+                    </div>
+                </div>
             </div>
 
-            <div class="nav_networks">
+            <div class="col-md-3">
+                <div class="card">
+                    <img src="../assets/img/switch.svg" class="card-img-top" alt="switch img">
+                    <div class="card-body">
+                        <h5 class="card-title">Cableado estructurado</h5>
+                        <p class="card-text">vanguardia de tendencias en comunicaciones corporativas</p>
+                        <a href="http://localhost/masterphp/Fabrica.cl/servicios/redes.php">leer Descripcion</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-3">
+                <div class="card">
+                    <img src="../assets/img/electricidad.png" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Ingenieria Electrica</h5>
+                        <p class="card-text">Conexiones en todas las areas que desees, consultanos</p>
+                        <a href="http://localhost/masterphp/Fabrica.cl/servicios/redes.php">Leer Descripcion</a>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+    </div>
+
+
+    <div class="clearfix"></div>
+    <footer id="footer">
+        <div class="footer_container">
+            <div class="nav_footer">
+                <h4>Customer</h4>
                 <ul>
                     <li>
-                        Manejo Especializado
+                        <a href="http://localhost/masterphp/Fabrica.cl/">Inicio</a>
+                    </li>
+
+                    <li>
+                        <a href="http://localhost/masterphp/Fabrica.cl/servicios/servicios.php">servicios</a>
                     </li>
                     <li>
-                        Instalaciones y actualizaciones
+                        <a href="http://localhost/masterphp/Fabrica.cl/#contacto">Contacto</a>
                     </li>
+
                     <li>
-                        Basados en normas Osi
-                    </li>
-                    <li>
-                        Certificaciones cisco
-                    </li>
-                    <li>
-                        Confianza y calidad
+                        <a href="http://localhost/masterphp/Fabrica.cl/#proyectos">Proyectos</a>
                     </li>
                 </ul>
             </div>
-        </div>
 
-        <div class="container">
-
-            <div class="row">
-                <div class="col-md-6">
-                    <div class="card">
-                        <img src="../assets/img/slider2.png" class="img-responsive" alt="...">
-                        <div class="card-body">
-                            <h5 class="card-title">Servicios electricos e instalaciones</h5>
-                            <p class="card-text">capacidades técnicas para dar una pronta solución, orientando, evaluando y ejecutando los proyectos de instalación
-                             de redes y comunicaciones que necesites</p>
-                            <a href="http://localhost/masterphp/Fabrica.cl/servicios/redes.php">leer Descripcion</a>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="col-md-3">
-                    <div class="card">
-                        <img src="../assets/img/switch.svg" class="card-img-top" alt="switch img">
-                        <div class="card-body">
-                            <h5 class="card-title">Cableado estructurado</h5>
-                            <p class="card-text">vanguardia de tendencias en comunicaciones corporativas</p>
-                            <a href="http://localhost/masterphp/Fabrica.cl/servicios/redes.php">leer Descripcion</a>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="col-md-3">
-                    <div class="card">
-                        <img src="../assets/img/electricidad.png" class="card-img-top" alt="...">
-                        <div class="card-body">
-                            <h5 class="card-title">Ingenieria Electrica</h5>
-                            <p class="card-text">Conexiones en todas las areas que desees, consultanos</p>
-                            <a href="http://localhost/masterphp/Fabrica.cl/servicios/redes.php">Leer Descripcion</a>
-                        </div>
-                    </div>
-                </div>
-
+            <div class="icon_footer">
+                <p class="icon">f</p>
+                <p class="icon">t</p>
+                <p class="icon">s</p>
+                <p class="icon">r</p>
             </div>
 
+            <div class="description_footer">
+                <p>
+                    Haz uso de nuestra mas reciente herramienta de calculos de proceso, basada
+                    en tecnologia de alta gama para ayudar a nuestros clientes.<br>
+                    !valoramos tu tiempo!
+                </p>
+            </div>
         </div>
 
 
-        <div class="clearfix"></div>
-        <footer id="footer">
-            <div class="footer_container">
-                <div class="nav_footer">
-                    <h4>Customer</h4>
-                    <ul>
-                        <li>
-                            <a href="http://localhost/masterphp/Fabrica.cl/">Inicio</a>
-                        </li>
 
-                        <li>
-                            <a href="http://localhost/masterphp/Fabrica.cl/servicios/servicios.php">servicios</a>
-                        </li>
-                        <li>
-                            <a href="http://localhost/masterphp/Fabrica.cl/#contacto">Contacto</a>
-                        </li>
-
-                        <li>
-                            <a href="http://localhost/masterphp/Fabrica.cl/#proyectos">Proyectos</a>
-                        </li>
-                    </ul>
-                </div>
-
-                <div class="icon_footer">
-                    <p class="icon">f</p>
-                    <p class="icon">t</p>
-                    <p class="icon">s</p>
-                    <p class="icon">r</p>
-                </div>
-
-                <div class="description_footer">
-                    <p>
-                        Haz uso de nuestra mas reciente herramienta de calculos de proceso, basada
-                        en tecnologia de alta gama para ayudar a nuestros clientes.<br>
-                        !valoramos tu tiempo!
-                    </p>
-                </div>
-            </div>
-
-
-
-        </footer>
+    </footer>
 
 
 
 
-        <script type="text/javascript" src="../bootstrap/jquery/jquery-3.5.1.min.js"></script>
-        <script type="text/javascript" src="../bootstrap/bootstrap/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="../bootstrap/jquery/jquery-3.5.1.min.js"></script>
+    <script type="text/javascript" src="../bootstrap/bootstrap/js/bootstrap.min.js"></script>
 
 
-        <script>
-            $(document).ready(function() {
-                $(".carousel").carousel({
-                    interval: 3500
-                });
+    <script>
+        $(document).ready(function() {
+            $(".carousel").carousel({
+                interval: 3500
             });
-        </script>
+        });
+    </script>
 
-    </body>
+</body>
 
 </html>
